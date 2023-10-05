00aaa53d3e80d11943d658796a4bd0dd9708bb64
VladimirRogachyov <142392938+VladimirRogachyov@users.noreply.github.com>
2023-10-05 15:42:55 +0700

    distant-changes

--- reository.learning/src/Main.java
+++ reository.learning/src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Репозиторий, обучение");
+        System.out.println("Репозиторий, обучение, изменение в удаленном");
     }
 }
